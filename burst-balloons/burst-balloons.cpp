9477e39009b7afb8a65b7cee20a910e382bde390
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-01-02 01:19:02 +0530

    Update burst-balloons.cpp

--- burst-balloons/burst-balloons.cpp
+++ burst-balloons/burst-balloons.cpp
@@ -28,25 +28,5 @@ class Solution {
          return dp[0][n - 1];
       }
 };
-
-int maxCoins(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(nums.size(),vector<int>(nums.size()));
-    
-        for(int gap=0;gap<n;gap++){
-            for(int i=0,j=gap;j<n;i++,j++){
-                cout<<i<<" "<<j<<endl;
-                int mx=0;
-                for(int k=i;k<=j;k++){
-                    int lr= k==i ? 0 : dp[i][k-1];
-                    int rr= k==j ? 0 : dp[k+1][j];
-                    int curr= (i==0?1:nums[i-1]) *(nums[k]) *(j==n-1?1:nums[j+1]);
-                    mx=max(mx,lr+rr+curr);
-                }
-                dp[i][j]=mx;
-            }
-        }
-        return dp[0][n-1];
-    }
-	// Refer this  video
-	// https://www.youtube.com/watch?v=YzvF8CqPafI&ab_channel=Pepcoding
+// Refer this  video
+// https://www.youtube.com/watch?v=YzvF8CqPafI&ab_channel=Pepcoding
