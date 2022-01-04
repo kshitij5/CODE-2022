64978d02d42d545e7bca45cb7e75ea9ad274666b
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-01-04 18:11:23 +0530

    Added solution - LeetHub

--- Overlapping rectangles - GFG/overlapping-rectangles.cpp
+++ Overlapping rectangles - GFG/overlapping-rectangles.cpp
@@ -10,15 +10,19 @@ using namespace std;
 class Solution {
   public:
    int doOverlap(int l1[], int r1[], int l2[], int r2[]) {
+       // the basic idea is to hypothesize that the rectangles never overlap
+       // and if its not true, we return true else false
        
-       // If one rectangle is on left side of other
+       // rectangles can never overlap if :
+       // one rectangle is on left side of other
        if (l1[0] > r2[0] || l2[0] > r1[0])
            return false;
 
-       // If one rectangle is above other
+       //one rectangle is above other
        if (r1[1] > l2[1] || r2[1] > l1[1])
            return false;
 
+        // else our hypothesis is wrong and rectangles indeed overlapp
    return true;
    }
    
