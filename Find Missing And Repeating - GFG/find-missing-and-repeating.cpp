57e10c3aa2463960902f7aa14af5ac2b36726ab7
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-01-06 21:33:10 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
@@ -6,25 +6,47 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
 public:
-    // using optimized map
-    int *findTwoElement(int *arr, int n) {
-        vector<int> map(n+1, 0);
+    // // using optimized map
+    // int *findTwoElement(int *arr, int n) {
+    //     vector<int> map(n+1, 0);
         
-        // marking frequency of a number
-        for(int i=0; i<n; i++) map[arr[i]] += 1;
+    //     // marking frequency of a number
+    //     for(int i=0; i<n; i++) map[arr[i]] += 1;
         
-        int *res=new int[2];
-        for(int i=1; i<=n; i++) {
-            int it = map[i];
+    //     int *res=new int[2];
+    //     for(int i=1; i<=n; i++) {
+    //         int it = map[i];
             
-            // if frequency is 0, its missing
-            if(it == 0) res[1] = i;
-            // if frequency is 2, its repeating
-            if(it == 2) res[0] = i;
-        }
+    //         // if frequency is 0, its missing
+    //         if(it == 0) res[1] = i;
+    //         // if frequency is 2, its repeating
+    //         if(it == 2) res[0] = i;
+    //     }
         
-        return res;
-    }
+    //     return res;
+    // }
+    
+    // using optimized inplace map
+    int *findTwoElement(int *arr, int n) {
+       int *res=new int[2];
+       
+       for(int i=0; i<n; i++) {
+           if(arr[abs(arr[i]) - 1] > 0) {
+               // from given constrain, we know 1 ≤ arr[i] ≤ n 
+               // and only one element is repeating, therefore, 
+               arr[abs(arr[i]) - 1] = -1 * arr[abs(arr[i]) - 1];
+           }else {
+               res[0] = abs(arr[i]);
+           }
+       }
+       
+       for(int i=0; i<n; i++) {
+           if(arr[i] > 0) {
+               res[1] = i+1;
+           }
+       }
+       return res;
+   }
 };
 
 // { Driver Code Starts.
