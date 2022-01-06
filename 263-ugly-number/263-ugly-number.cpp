90ca7ce6b6b97b3ea77f6274edef86e7d129d3f7
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-01-06 20:57:07 +0530

    Time: 4 ms (33.85%), Space: 5.9 MB (20.18%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     // simple brute force
-    bool isUgly(int num) {
+    // bool isUgly(int num) {
+    //     if (num <= 0) return false;
+    //     while (num % 2 == 0) num /= 2;
+    //     while (num % 3 == 0) num /= 3;
+    //     while (num % 5 == 0) num /= 5;
+    //     return num == 1;
+    // }
+    
+    // better solution
+    bool isUgly(int num){
         if (num <= 0) return false;
-        while (num % 2 == 0) num /= 2;
-        while (num % 3 == 0) num /= 3;
-        while (num % 5 == 0) num /= 5;
-        return num == 1;
+        while(num % 3 == 0) num /= 3;
+        while(num % 5 == 0) num /= 5;
+        return (num  & (num - 1)) == 0;
     }
 };
