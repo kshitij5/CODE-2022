05bed750bd73ef8f6adf671346fc3a42750b22d1
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-01-06 12:40:21 +0530

    Time: 63 ms (21.12%), Space: 20.5 MB (55.45%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    // using map
+    // tc - o(n)
+    // sc - o(n)
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> map;
+        
+        // store anagrams in group using map
+        for(string s: strs) {
+            string key = s;
+            
+            // sorting to generate key
+            sort(key.begin(), key.end());
+            
+            map[key].push_back(s);
+        }
+        
+        vector<vector<string>> res;
+        
+        // extract all the grouped anagrams
+        for(auto it: map) res.push_back(it.second);
+        
+        return res;
+    }
+};
