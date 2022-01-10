78c7721f963d69ef67de4e49df1f0208f18dfbfc
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-01-10 17:06:00 +0530

    Time: 0 ms (100.00%), Space: 6 MB (13.03%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -3,6 +3,12 @@ public:
     // brute force
     // tc - o(1) // since at most 32 iteration will be done
     // sc - o(1)
+    
+    // what we are doing is basically, converting bin to dec 
+    // but instead, multiplying when digit is 0 and not 1
+    // (obviously since we are finding complement)
+    // eg - >>> 1       0       1       0   = 10
+    //          2*0 +  2*1  +  2*0  +  2*3  = 5
     int bitwiseComplement(int n) {
 	//only one case is exception because we can't handle  0 in while loop
         if(n == 0) return 1;
@@ -16,14 +22,4 @@ public:
         }
         return ans;
     }
-    
-	// // bit manupulation (doesn't work, obviously for even numbers)
-	// int bitwiseComplement(int n) {
-	// //only one case is exception because we can't handle  0 in while loop
-	// if(n == 0) return 1;
-	// // when all bits be set, we have to return zeroe
-	// if(((n + 1) & n) == 0) return 0;
-	// // all are we doing is right shifting a bit of n
-	// return n >> 1;
-	// }
 };
