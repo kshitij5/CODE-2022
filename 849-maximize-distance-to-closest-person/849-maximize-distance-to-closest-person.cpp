6d2aa186c44746d1edf2108b5ff5ef07fbb87026
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-01-16 23:31:51 +0530

    Time: 26 ms (15.11%), Space: 17.1 MB (63.67%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
+    // using two pointers
+    // tc - o(n)
+    // sc - o(1)
     int maxDistToClosest(vector<int>& seats) {
         // there can be three condition
-        ///max empty is at left
-        /// max empty is at right
-        ///max empty is at middle anywhere
+        /// 1. max empty is at left
+        /// 2. max empty is at right
+        /// 3. max empty is at middle anywhere
         int lastfilled = -1;
         int pos = 0;
         
         int n = seats.size();
         for(int i=0; i<n; i++) {
             if(seats[i]) {
+                // handling 1st case
                 if(lastfilled == -1) {
                     pos = i;
                 }
+                // else 3rd case
                 pos = max((i-lastfilled)/2, pos);
                 lastfilled = i;
             }
         }
         
+        // handling 2nd case
         if(lastfilled < n-1) {
             pos = max(pos, n - lastfilled - 1);
         }
