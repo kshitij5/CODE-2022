28952e2be43d579c2eb88c742c5dcdca479e38b7
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-01-22 23:00:37 +0530

    Time: 77 ms (52.35%), Space: 6.4 MB (75.17%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    // using dp
+    // tc - o(sqrt(n) * n)
+    // sc - o(n)
+    bool winnerSquareGame(int n) {
+        if(n==0)
+            return false;
+			
+        vector<bool> dp(n+1, false);        
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j*j<=i; j++)
+            {
+                if(dp[i-(j*j)]==false)
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+};
