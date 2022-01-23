4bf2ef48394d43df8074f1c582a05ee772063c82
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-01-23 21:33:42 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (36.79%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int len(int n) {
+        int size = 0;
+        while(n) {
+            n/=10;
+            size++;
+        }
+        return size;
+    }
+    
+    // brute forced
+    // tc - o(no. of sequential digits)
+    // sc - o(1)
+    vector<int> sequentialDigits(int low, int high) {
+        int first[] = {0, 1, 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789};
+        int factors[] = {0, 1, 11, 111, 1111, 11111, 111111, 1111111, 11111111, 111111111};
+        int mn = len(low), mx = len(high);
+        
+        vector<int> res;
+        for(int k=mn; k<=min(mx+1, 9); k++) {
+            int factor = factors[k];
+            int turns = 9-k+1;
+            int next = first[k];
+            while(turns-- && next <= high) {
+                if(next >= low)
+                    res.push_back(next);
+                next += factor;
+            }
+            
+            if(next > high) break;
+        }
+        return res;
+    }
+};
