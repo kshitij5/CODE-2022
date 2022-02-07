e2b5bcf75e1ae4c7bb6fb407538146739e9f80f6
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-02-07 23:49:27 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (67.00%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
+    // brute force
+    // tc - 0(nlog n)
+    // sc - o(1)
+//     char findTheDifference(string s, string t) {
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+        
+//         for(int i=0; i<t.size(); i++) {
+//             if(s[i] != t[i]) {
+//                 return t[i];
+//             }
+//         }
+        
+//         return t[t.size()-1];
+//     }
+    
+    // optimized
+    // tc - 0(n)
+    // sc - o(1)
     char findTheDifference(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        
-        for(int i=0; i<t.size(); i++) {
-            if(s[i] != t[i]) {
-                return t[i];
-            }
+        int count = 0;
+        for(auto i: t){
+            count += (i-'a');
         }
+		
+        for(auto i: s) count -= (i-'a');
         
-        return t[t.size()-1];
+        return 'a' + count;
     }
 };
