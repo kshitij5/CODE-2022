795dcf877a130f6ab6e5600cfceac4cc384a1389
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-02-08 12:12:36 +0530

    Time: 3 ms (53.62%), Space: 5.8 MB (75.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     // brute force
-    string sumOfDigits(string s) {
-        int sum = 0;
-        for(char c: s) {
-            sum += c - '0';
-        }
-        cout << sum << endl;
-        return to_string(sum);
-    }
-    int addDigits(int num) {
-        string str = to_string(num);
-        while(str.size()>1) {
-            str = sumOfDigits(str);
-        }
+    // tc - o(n)
+    // sc - o(1)
+//     string sumOfDigits(string s) {
+//         int sum = 0;
+//         for(char c: s) {
+//             sum += c - '0';
+//         }
+//         cout << sum << endl;
+//         return to_string(sum);
+//     }
+//     int addDigits(int num) {
+//         string str = to_string(num);
+//         while(str.size()>1) {
+//             str = sumOfDigits(str);
+//         }
         
-        return str[0] - '0';
+//         return str[0] - '0';
+//     }
+    
+    // using digital root
+    // tc - o(1)
+    // sc - o(1)
+    int addDigits(int num) {
+       if(num==0)
+           return 0;
+        else if(num % 9 == 0)
+            return 9;
+     return num % 9;
     }
 };
