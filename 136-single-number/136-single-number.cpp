adb55076b34e0526fb1d990d7cf284a8c01e3b77
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-02-15 13:14:14 +0530

    Time: 43 ms (18.65%), Space: 17 MB (19.26%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     // brute force
+//     int singleNumber(vector<int>& nums) {
+//         if(nums.size()==1) return nums[0];
+        
+//         unordered_map<int, int> mp;
+//         for(int i: nums) mp[i]++;
+        
+//         for(auto it: mp) {
+//             if(it.second == 1) return it.first;
+//         }
+//         return nums[0];
+//     }
+    
+    // using xor
     int singleNumber(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
-        
-        unordered_map<int, int> mp;
-        for(int i: nums) mp[i]++;
-        
-        for(auto it: mp) {
-            if(it.second == 1) return it.first;
-        }
-        return nums[0];
+        int n = nums.size();
+        //xor all numbers, the left over number would be the non repeated one
+         // since the equl numbers cancel out each others bits
+         int num = 0;
+         for (int i = 0; i < n; ++i) {
+             num ^= nums[i];
+         }
+        return num;
     }
 };
