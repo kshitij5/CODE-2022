406a6c5017b4d56031a2facb6a634983f90f9c1f
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-02-21 14:26:22 +0530

    Time: 14 ms (92.51%), Space: 19.6 MB (52.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
+    // brute force
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map<int,  int> mp;
+        
+//         for(int i: nums) mp[i]++;
+        
+//         int mx = INT_MIN, res = 0;
+//         for(auto it: mp) {
+//             if(it.second > mx) {
+//                 mx = it.second;
+//                 res = it.first;
+//             }
+//         }
+        
+//         return res;
+//     }
+    
+    // using boyce moore majority voting algo
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,  int> mp;
-        
-        for(int i: nums) mp[i]++;
-        
-        int mx = INT_MIN, res = 0;
-        for(auto it: mp) {
-            if(it.second > mx) {
-                mx = it.second;
-                res = it.first;
-            }
+        int count=0, res = 0;
+        for (int num: nums) {
+            if (count == 0)
+                res = num;
+            if (num != res)
+                count--;
+            else
+                count++;
         }
-        
         return res;
     }
 };
