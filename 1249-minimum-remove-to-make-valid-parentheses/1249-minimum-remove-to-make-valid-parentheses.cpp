11c5dfa8c752d4b35002f688a0c42429206d27b9
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-03-16 00:32:08 +0530

    Time: 42 ms (29.90%), Space: 11.8 MB (42.01%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+//     string minRemoveToMakeValid(string s) {
+//         string res;
+//         int count = 0;
+        
+//         for(char c: s) {
+//             if(c=='(') count++;
+//             else if(c==')') count--;
+//             else {
+//                 res.push_back(c);
+//                 continue;
+//             }
+            
+//             if(count>=0) res.push_back(c);
+//         }
+        
+//         return res;
+//     }
+    
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        
+        // Iterate over string and mark bad closing parentheses as defect using '*'
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(') st.push(i);
+            else if(s[i]==')'){
+                if(st.size()) st.pop();
+                else s[i]='*';
+            }
+        }
+        
+        // Iterate over stack to mark bad opening parentheses as defect using '*'
+        while(!st.empty()){
+            s[st.top()] = '*';
+            st.pop();
+        }
+        
+        // Remove defected parentheses from string
+        string res;
+        for(char c: s) if(c!='*')res += c;
+        
+        return res;
+    }
+};
