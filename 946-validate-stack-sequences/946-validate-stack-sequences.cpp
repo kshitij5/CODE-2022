1531f9c7fb10aaa0671119ec32f3a62cd063afe1
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-03-16 22:11:07 +0530

    Time: 8 ms (83.81%), Space: 15.3 MB (59.16%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
+    // initial solution
+//     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+//         stack<int> st;
+//         st.push(INT_MIN);
+//         int i = 0;
+        
+//         for(int last_popped: popped) {
+//             if(st.top()!=last_popped) {
+//                 while(i<pushed.size() && pushed[i]!=last_popped) {
+//                     st.push(pushed[i++]);
+//                 }
+//                 i++;
+//                 continue;
+//             }
+//             st.pop();
+//         }
+        
+//         return st.size()==1;
+//     }
+    
+    // intution method using stack
+    // tc - o(n)
+    // sc - o(n)
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         stack<int> st;
-        st.push(INT_MIN);
-        int i = 0;
+        int j = 0;
         
-        for(int last_popped: popped) {
-            if(st.top()!=last_popped) {
-                while(i<pushed.size() && pushed[i]!=last_popped) {
-                    st.push(pushed[i++]);
-                }
-                i++;
-            }else{
+        for(auto val : pushed){
+            st.push(val);
+            while(st.size() > 0 && st.top() == popped[j]){
                 st.pop();
+                j++;
             }
         }
-        
-        return st.size()==1;
+        return st.empty();
     }
 };
+
