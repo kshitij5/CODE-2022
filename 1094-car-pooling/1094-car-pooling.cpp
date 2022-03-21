6f7ee094e7b76437d4ab4373436f04808b5d3640
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-03-21 23:05:19 +0530

    Time: 32 ms (26.82%), Space: 11.1 MB (20.90%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    // using map
+    // using map stl
     // tc - o(n)
-    // sc - o(1)
+    // sc - o(n)
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         map<int, int> mp;
-        mp.clear();
         
-         for(auto it: trips) {
+        for(auto it: trips){
             int person = it[0];
-            int from = it[1];
-            int to = it[2];
-            // no. of person that will enter the car at the 'from' position
-            // {start, persons}
-            mp[from] += person;     
-			                            
-            // no. of person that will leave the car at that 'to' position
-            // {end, -persons}
-            mp[to] -= person;     
-			                               
+            mp[it[1]] += person;     // no. of person that will enter the car at the 'from' position
+			                               // {start, persons}
+            mp[it[2]] -= person;     // no. of person that will leave the car at that 'to' position
+			                               // {end, -persons}
         }
         
         int person = 0;
+        
+        for(auto it: mp) {
+            cout << it.first << " " << it.second << endl;
+        }
+        
         for(auto x : mp){
             person += x.second;
-            // checking if person exceed the required capacity 
-            if(person > capacity){     
+            if(person > capacity) {     // checking if person exceed the required capacity 
                 return false;
-            }
-            else if(person < 0){
-                person = 0;
-            }
+            } 
         }
         
         return true;
