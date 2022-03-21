5206ea055489860531eb44ccd98dbb806f3a1ffe
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-03-21 22:07:51 +0530

    Time: 4 ms (72.57%), Space: 6.7 MB (64.08%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.size();
+        vector<int> res;
+        vector<int> lastloc(26);
+        
+        for(int i=0; i<n; i++) {
+            lastloc[s[i]-'a'] = i;
+        }
+        
+        int left = 0;
+        int maxlength = lastloc[s[0]-'a'];
+        
+        for(int i=1; i<n; i++) {
+            if(i>maxlength) {
+                // cout<<s[i] << maxlength << endl;
+                res.push_back(maxlength-left+1);
+                left = i;
+                maxlength = lastloc[s[i]-'a'];
+            }
+            maxlength = max(maxlength, lastloc[s[i]-'a']);
+        }
+        res.push_back(maxlength-left+1);
+        
+        return res;
+    }
+};
