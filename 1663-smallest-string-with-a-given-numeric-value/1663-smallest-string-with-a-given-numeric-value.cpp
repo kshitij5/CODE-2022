d450f1e127a0b2ec75bb8847dd862a035fae4e7e
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-03-23 00:14:42 +0530

    Time: 52 ms (66.08%), Space: 27.2 MB (35.89%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-//     string getSmallestString(int n, int k) {
-//         string res;
-//         int i=n-1, sum = n;
-//         while(n--)res+='a';
-        
-//         while(sum < k) {
-//             if((sum+25) <= k) {
-//                 sum += 25;
-//                 res[i--] = 'z';
-//                 continue;
-//             }
-//             res[i--] = ((k-sum)+'a');
-//             sum += (k-sum);
-//         }
-//         return res;
-//     }
-    
     string getSmallestString(int n, int k) {
         string res;
-        int i = n-1, sum = n;
+        int i=n-1, sum = n;
+        
         while(n--)res+='a';
+        // n=3, res = aaa
         
         while(sum < k) {
-            int min = std::min(25, (k-sum));
-            res[i--] = (min + 'a');
-            sum +=  min;
+            if((sum+25) <= k) {
+                sum += 25;
+                res[i--] = 'z';
+                continue;
+            }
+            res[i--] = ((k-sum)+'a');
+            sum += (k-sum);
         }
+        
         return res;
     }
+    
+//     string getSmallestString(int n, int k) {
+//         string res;
+//         int i = n-1, sum = n;
+//         while(n--)  res+='a';
+        
+//         while(sum < k) {
+//             int min = std::min(25, (k-sum));
+//             res[i--] = (min + 'a');
+//             sum +=  min;
+//         }
+        
+//         return res;
+//     }
 };
