0a6521ab8ca54b68a1028f715c8828d50fa4ff35
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-03-24 13:04:04 +0530

    Time: 76 ms (87.76%), Space: 41.9 MB (57.42%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
+    // using two pointers approach
+    // tc - o(nlogn)
+    // sc - o(1)
     int numRescueBoats(vector<int>& people, int limit) {
         int n = people.size();
         int res = 0;
         
+        // sorting people according to their weights
         sort(people.begin(), people.end());
         int left = 0, right = n-1;
         
+        //  we will try to make pairs of people having
+        // lightest and heaviest weight such that its under limit
+        // or else they'll go alone
         while(left <= right) {
-            if(people[left]+people[right] > limit) {
-                res++;
+            
+            // if sum is over the limit,
+            // heaviest will go alone.
+            if(people[left] + people[right] > limit) {
                 right--;
             } else {
-                res++;
-                left++;
-                right--;
+            // if sum is under the limit,
+            // both heaviest and lightest will go together.
+                left++, right--;
             }
+            res++;
         }
         
         return res;
