e7cb4e419effb6da040f16b846da6132dbddcc82
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-04-09 13:50:15 +0530

    Time: 16 ms (82.46%), Space: 13.8 MB (41.62%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -3,23 +3,43 @@ public:
     static bool cmp(pair<int, int>& a, pair<int, int>& b) {
         return a.second > b.second;
     }
-    // brute force
+//     // brute force
+//     // tc - o(nlogn)
+//     // sc - o(n)
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int, int> freq;
+//         for(int i: nums) freq[i]++;
+        
+//         vector<pair<int, int>> srt;
+//         for(auto it: freq)srt.push_back({it.first, it.second});
+        
+//         sort(srt.begin(), srt.end(), cmp);
+        
+//         vector<int> ans;
+//         for(auto it: srt) {
+//             ans.push_back(it.first);
+//             k--;
+//             if(k<=0) break;
+//         }
+        
+//         return ans;
+//     }
+    
+    
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> freq;
         for(int i: nums) freq[i]++;
         
-        vector<pair<int, int>> srt;
-        for(auto it: freq)srt.push_back({it.first, it.second});
+        priority_queue<pair<int, int>> pq;
+        for(auto it: freq) pq.push({it.second, it.first});
         
-        sort(srt.begin(), srt.end(), cmp);
-        
-        vector<int> ans;
-        for(auto it: srt) {
-            ans.push_back(it.first);
-            k--;
-            if(k<=0) break;
+        vector<int> res;
+        while(k--) {
+            pair<int, int> i = pq.top();
+            res.push_back(i.second);
+            pq.pop();
         }
         
-        return ans;
+        return res;
     }
 };
