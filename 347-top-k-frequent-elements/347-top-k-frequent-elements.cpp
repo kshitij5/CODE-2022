b0a593e1c64cb0df29d2f2317e663f297b2dcba5
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-04-09 14:21:39 +0530

    Time: 22 ms (51.02%), Space: 15.4 MB (5.22%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -26,18 +26,46 @@ public:
 //     }
     
     
+//     // using priority queue
+//     // tc - o(nlogn)
+//     // sc - o(n)
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int, int> freq;
+//         for(int i: nums) freq[i]++;
+        
+//         priority_queue<pair<int, int>> pq;
+//         for(auto it: freq) pq.push({it.second, it.first});
+        
+//         vector<int> res;
+//         while(k--) {
+//             pair<int, int> i = pq.top();
+//             res.push_back(i.second);
+//             pq.pop();
+//         }
+        
+//         return res;
+//     }
+    
+    // using bucket sort
+    // tc - o(n)
+    // sc - o(n)
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
         unordered_map<int, int> freq;
         for(int i: nums) freq[i]++;
         
-        priority_queue<pair<int, int>> pq;
-        for(auto it: freq) pq.push({it.second, it.first});
+        vector<vector<int>> buckets(n+1);
+        for(auto it: freq) {
+            buckets[it.second].push_back(it.first);
+        }
         
         vector<int> res;
-        while(k--) {
-            pair<int, int> i = pq.top();
-            res.push_back(i.second);
-            pq.pop();
+        while(k>0) {
+            auto bucket = buckets.back();
+            
+            for(int i: bucket)
+                res.push_back(i), k--;
+            buckets.pop_back();
         }
         
         return res;
