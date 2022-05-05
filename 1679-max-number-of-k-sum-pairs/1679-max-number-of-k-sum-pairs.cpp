7a090a41f87b518e45537368dc913f910012d836
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-05-05 09:24:03 +0530

    Time: 181 ms (59.91%), Space: 67.6 MB (43.82%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -23,25 +23,57 @@ public:
 //         return count;
 //     }
     
-    int maxOperations(vector<int>& nums, int k) {
-        int count = 0, n = nums.size();
+//     // two pointers approach
+//     // tc - o(nlogn)
+//     // sc - o(1)
+//     int maxOperations(vector<int>& nums, int k) {
+//         int count = 0, n = nums.size();
         
-        sort(nums.begin(), nums.end());
-        int left=0, right = n-1;
+//         sort(nums.begin(), nums.end());
+//         int left=0, right = n-1;
         
-        while(left < right) {
-            int sum = nums[left] + nums[right];
+//         while(left < right) {
+//             int sum = nums[left] + nums[right];
             
-            if(sum > k) {
-                right--;
-            } else if( sum < k) {
-                left++;
-            } else {
-                left++, right--;
-                count++;
+//             if(sum > k) {
+//                 right--;
+//             } else if( sum < k) {
+//                 left++;
+//             } else {
+//                 left++, right--;
+//                 count++;
+//             }
+//         }
+        
+//         return count;
+//     }
+    
+    // using map and disjoint sum pair formula
+    // tc - o(n)
+    // sc - o(n)
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        int count = 0;
+        
+        for(int i: nums) {
+            map[i]++;
+        }
+        
+        for(auto i: map) {
+            int x = i.first;
+            if(map.find(k-x) != map.end()) {
+                if(k-x != x) {
+                    count += min(i.second, map[k-x]);
+                    map[x] = 0;
+                    map[k-x] = 0;
+                } else {
+                    count += floor(i.second/2);
+                    map[x] = 0;
+                }
             }
         }
         
         return count;
     }
+    
 };
