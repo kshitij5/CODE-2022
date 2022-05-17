6e7dab3d7d4d919e6f906a41deaf1c385e526147
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-05-17 17:00:49 +0530

    Time: 919 ms (15.40%), Space: 164 MB (22.94%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -10,24 +10,44 @@
 
 class Solution {
 public:
-    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        queue<TreeNode*> q;
-        q.push(cloned);
+    // bfs
+//     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+//         queue<TreeNode*> q;
+//         q.push(cloned);
         
-        while(!q.empty()) {
-            int size = q.size();
+//         while(!q.empty()) {
+//             int size = q.size();
             
-            for(int i=0; i<size; i++) {
-                TreeNode* temp = q.front();
-                q.pop();
+//             for(int i=0; i<size; i++) {
+//                 TreeNode* temp = q.front();
+//                 q.pop();
                 
-                if(temp->val == target->val) return temp;
+//                 if(temp->val == target->val) return temp;
                 
-                if(temp->left != NULL) q.push(temp->left);
-                if(temp->right != NULL) q.push(temp->right);
-            }
+//                 if(temp->left != NULL) q.push(temp->left);
+//                 if(temp->right != NULL) q.push(temp->right);
+//             }
+//         }
+        
+//         return NULL;
+//     }
+    
+    //  dfs
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        if(!cloned) return NULL;
+        
+        if(target->val == cloned->val) {
+            return cloned;
         }
         
+        TreeNode* left = getTargetCopy(original, cloned->left, target);
+        if(left!=NULL) {
+            return left;
+        }
+        TreeNode* right = getTargetCopy(original, cloned->right, target);
+        if(right!=NULL) {
+            return right;
+        }
         return NULL;
     }
 };
