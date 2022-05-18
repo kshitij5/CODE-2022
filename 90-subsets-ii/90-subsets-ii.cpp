22136b014c5f2fa2b50bbd2fd8683d248624bc64
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-05-19 00:05:07 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (73.23%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
-    void f(int n, vector<int> &arr, vector<int>& temp, set<vector<int>>& res, int i) {
-        // base case
-        if(i==n) {
-            res.insert(temp);
-            return;
-        }
+    // using set
+    // tc - : O( 2^n *(k log (x) ))
+//     void f(int n, vector<int> &arr, vector<int>& temp, set<vector<int>>& res, int i) {
+//         // base case
+//         if(i==n) {
+//             res.insert(temp);
+//             return;
+//         }
 
-        temp.push_back(arr[i]);
-        f(n, arr, temp, res, i+1);
-        temp.pop_back();
-        f(n, arr, temp, res, i+1);
+//         temp.push_back(arr[i]);
+//         f(n, arr, temp, res, i+1);
+//         temp.pop_back();
+//         f(n, arr, temp, res, i+1);
+
+//         return;
+//     }
+    void f(int n, vector<int> &arr, vector<int>& temp, vector<vector<int>>& res, int idx) {
+        res.push_back(temp);
+        
+        for(int i = idx; i<n; i++) {
+            if (i != idx && arr[i] == arr[i - 1]) continue; 
+            temp.push_back(arr[i]);
+            f(n, arr, temp, res, i+1);
+            temp.pop_back();
+        }
 
         return;
     }
-
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         int n = nums.size();
-        set<vector<int>> res;
+        vector<vector<int>> res;
         vector<int> t;
         sort(nums.begin(), nums.end());
         f(n, nums, t, res, 0);
-        vector<vector<int>> ans(res.begin(), res.end());
-        return ans;
+        // vector<vector<int>> ans(res.begin(), res.end());
+        return res;
     }
 };
