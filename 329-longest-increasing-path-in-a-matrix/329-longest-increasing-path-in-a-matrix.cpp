726fe52314d323b174be21182a7790a1c1acfc3d
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-05-20 01:01:22 +0530

    Time: 55 ms (69.58%), Space: 16.1 MB (55.62%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-//     // dfs
+//     // dfs [gives tle]
 //     // tc - o(m*n * 4^(m*n))
 //     // m*n -> double for loop
 //     // 4^(m*n) -> for each value we can traverse further in 4directionly each m*n at worst case
@@ -11,7 +11,7 @@ public:
 //         for(auto it: coordinates) {
 //             int x = it.first+i, y = it.second+j;
             
-//             if(x>=m || y>=n || x<0 || y<0 || matrix[x][y] <= matrix[i][j]) continue;
+//             if(x>=n || y>=m || x<0 || y<0 || matrix[x][y] <= matrix[i][j]) continue;
 //                 mx = max(mx, 1 + dfs(matrix, x, y, m, n));
 //         }
         
@@ -36,18 +36,18 @@ public:
     // sc : O(n*m)
     vector<pair<int, int>> coordinates = {{-1,0},{1,0},{0,-1},{0,1}};
     vector<vector<int> > dp; // dp[i][j] will store maximum path length starting from matrix[i][j]
-    int maxPath, n, m;
+    
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        maxPath = 0, n = size(matrix), m = size(matrix[0]);
+        int maxPath = 0, n = size(matrix), m = size(matrix[0]);
         dp.resize(n, vector<int>(m));
         // calculating maximum path from each cell and at last returning the maximum length
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++)
-                maxPath = max(maxPath, solve(matrix, i, j));            
+                maxPath = max(maxPath, solve(matrix, i, j, n ,m));            
         return maxPath;
     }
     // recursive solver for each cell with dp for storing each calculated result
-    int solve(vector<vector<int>>& mat, int i, int j){
+    int solve(vector<vector<int>>& mat, int i, int j, int n, int m){
         if(dp[i][j]) return dp[i][j]; // return if result is already calculated
         dp[i][j] = 1;  // minimum path from each cell is always atleast 1
         // choosing each possible move available to us
@@ -56,7 +56,7 @@ public:
             // bound checking as well as move to next cell only when it is greater in value
             if(x < 0 || y < 0 || x >= n || y >= m || mat[x][y] <= mat[i][j]) continue;
             // max( current optimal, select current + optimal solution after moves[k] from current cell
-            dp[i][j] = max(dp[i][j], 1 + solve(mat, x, y));
+            dp[i][j] = max(dp[i][j], 1 + solve(mat, x, y, n, m));
         }         
         return dp[i][j];
     }
