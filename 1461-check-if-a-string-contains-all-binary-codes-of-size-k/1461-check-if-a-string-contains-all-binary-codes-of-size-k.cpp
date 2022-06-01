e1ae0698f8264197f3ad2ceb0b08d92125550535
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-06-01 10:35:50 +0530

    Time: 1832 ms (5.27%), Space: 91.4 MB (14.91%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    // brute force
+    // Time Complexity : O(N*k), where N is the size of string and k is length of binary code.
+    // Space Complexity : O(N*k)
     bool hasAllCodes(string s, int k) {
         if(s.size() <= k) return false;
         set<string> codes;
@@ -10,6 +13,8 @@ public:
             // cout << str << endl;
         }
         
-        return codes.size() == pow(2, k);
+        return codes.size() == (1 << k);
     }
+    
+    
 };
