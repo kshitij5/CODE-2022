45f2c716d24f994e0a99f813712b44d2f971d1a0
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-07-25 14:24:48 +0530

    Time: 3 ms (91.42%), Space: 9.6 MB (5.10%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -14,28 +14,55 @@ public:
 //         return found;
 //     }
     
-    // better optimised, binary search
-    // Time complexity: O(Log m + Log n)
-    // Space complexity: O(1)
-
+    // binary search
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size(), m = matrix[0].size();
-        int lower = 0, higher = n*m-1;
         
-        while(lower<=higher) {
-            int mid = lower + (higher - lower)/2;
+        for(int i=0;i<n;i++) {
+            int lower = 0, higher = m-1;
             
-            int val = matrix[mid/m][mid%m];
-            
-            if(val > target) {
-                higher = mid-1;
-            } else if(val < target) {
-                lower = mid+1;
-            } else {
-                return true;
+            if(target>=matrix[i][0] && target<=matrix[i][m-1]) {
+                while(lower<=higher) {
+                    int mid = lower + (higher - lower)/2;
+
+                    int val = matrix[i][mid];
+
+                    if(val > target) {
+                        higher = mid-1;
+                    } else if(val < target) {
+                        lower = mid+1;
+                    } else {
+                        return true;
+                    }
+                }
             }
         }
         
         return false;
     }
+    
+    // better optimised, binary search
+    // Time complexity: O(Log m + Log n)
+    // Space complexity: O(1)
+
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int n = matrix.size(), m = matrix[0].size();
+//         int lower = 0, higher = n*m-1;
+        
+//         while(lower<=higher) {
+//             int mid = lower + (higher - lower)/2;
+            
+//             int val = matrix[mid/m][mid%m];
+            
+//             if(val > target) {
+//                 higher = mid-1;
+//             } else if(val < target) {
+//                 lower = mid+1;
+//             } else {
+//                 return true;
+//             }
+//         }
+        
+//         return false;
+//     }
 };
