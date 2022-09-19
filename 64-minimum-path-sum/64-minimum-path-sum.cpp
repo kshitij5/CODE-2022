01b794b75fc8bb023f002bdec73c88ee9dbc76b0
Avishek Kumar Sharma <44705390+kshitij5@users.noreply.github.com>
2022-09-19 22:00:43 +0530

    Time: 12 ms (75.26%), Space: 10.1 MB (31.06%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,27 +1,51 @@
 class Solution {
 public:
     
-	int f(int i, int j, int n, int m, vector<vector<int>>& grid, vector<vector<int>>&dp) {
-	    // base case
-	    if(i==n-1 && j==m-1) return grid[i][j];
+// 	int f(int i, int j, int n, int m, vector<vector<int>>& grid, vector<vector<int>>&dp) {
+// 	    // base case
+// 	    if(i==n-1 && j==m-1) return grid[i][j];
 	    
-	    if(i<0 || j<0 || i>=n || j>=m) return 1e9;
-	    
-	    
-	    if(dp[i][j]!=-1) return dp[i][j];
-	    
-	    int right = f(i, j+1, n, m, grid, dp);
-	    int down = f(i+1, j, n, m, grid, dp);
+// 	    if(i<0 || j<0 || i>=n || j>=m) return 1e9;
 	    
 	    
-	    return dp[i][j] = grid[i][j]+min(down, right);
+// 	    if(dp[i][j]!=-1) return dp[i][j];
 	    
-	}
+// 	    int right = f(i, j+1, n, m, grid, dp);
+// 	    int down = f(i+1, j, n, m, grid, dp);
+	    
+	    
+// 	    return dp[i][j] = grid[i][j]+min(down, right);
+	    
+// 	}
+//     int minPathSum(vector<vector<int>>& grid) {
+        
+//         int n = grid.size(), m = grid[0].size();
+//         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        
+//         return f(0, 0, n, m, grid, dp);
+//     }
+    
+    
     int minPathSum(vector<vector<int>>& grid) {
         
         int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
         
-        return f(0, 0, n, m, grid, dp);
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(i==0 && j==0) {
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
+                
+                int left = INT_MAX, up = INT_MAX;
+                if(i>0) up = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+                
+                dp[i][j] = grid[i][j] + min(up, left);
+            }
+        }
+        
+        return dp[n-1][m-1];
     }
 };
